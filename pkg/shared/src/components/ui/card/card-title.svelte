54f34c5e303f285545bb6714a3dc53607be85172
Hadrien Grandry <hadrien.dev@hgrandry.net>
2025-08-11 11:08:01 +0900

    Fix cards issues

--- pkg/shared/src/components/ui/card/card-title.svelte
+++ pkg/shared/src/components/ui/card/card-title.svelte
@@ -1,19 +1,14 @@
 <script lang="ts">
-	import { cn } from "../../../lib/utils.js";
+	import { cn } from '../../../lib/utils.js';
 
 	interface Props {
 		class?: string;
+		[key: string]: any;
 	}
 
-	let {
-		class: className,
-		...restProps
-	}: Props = $props();
+	let { class: className, children, ...restProps }: Props = $props();
 </script>
 
-<h3
-	class={cn("text-2xl font-semibold leading-none tracking-tight", className)}
-	{...restProps}
->
-	<slot />
+<h3 class={cn('text-2xl font-semibold leading-none tracking-tight', className)} {...restProps}>
+	{@render children?.()}
 </h3>
--- pkg/shared/src/components/ui/card/card.svelte
+++ pkg/shared/src/components/ui/card/card.svelte
@@ -1,22 +1,14 @@
 <script lang="ts">
-	import { cn } from "../../../lib/utils.js";
+	import { cn } from '../../../lib/utils.js';
 
 	interface Props {
 		class?: string;
+		[key: string]: any;
 	}
 
-	let {
-		class: className,
-		...restProps
-	}: Props = $props();
+	let { class: className, children, ...restProps }: Props = $props();
 </script>
 
-<div
-	class={cn(
-		"rounded-lg border bg-card text-card-foreground shadow-sm",
-		className
-	)}
-	{...restProps}
->
-	<slot />
+<div class={cn('bg-card text-card-foreground rounded-lg border shadow-sm', className)} {...restProps}>
+	{@render children?.()}
 </div>
